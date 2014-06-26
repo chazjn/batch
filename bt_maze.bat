48c056791e93d4e86b99e7b52ab90e7479467645
chazjn <chaz.newman@gmail.com>
2014-06-26 21:42:54 +0100

    Moved things around...

--- bt_maze.bat
+++ bt_maze.bat
@@ -2,6 +2,11 @@
 cls
 setlocal EnableDelayedExpansion
 
+echo [Binary Tree Algorithm Maze Creator] @chazjn 26/06/2014. ver 1.1.
+echo Based on code here: http://weblog.jamisbuck.org/2011/2/7/maze-generation-algorithm-recap
+echo bt_maze /? for more options
+echo/
+
 :: Get command flags or use defaults
 :: %1 = Width
 :: %2 = Height
@@ -30,9 +35,9 @@ For /L %%h IN (1, 1, %maxheight%) DO (
 			IF DEFINED west IF NOT DEFINED north (SET go=2)
 			IF DEFINED north IF DEFINED west (SET /a go=!random! %% 2 + 1)
 			
-			IF !go!==0 (SET go=Â¦Â¯)
-			IF !go!==1 (SET go=Â¦ )
-			IF !go!==2 (SET go=Â¯Â¯)
+			IF !go!==0 (SET go=Ýî)
+			IF !go!==1 (SET go=Ý )
+			IF !go!==2 (SET go=îî)
 			
 			SET MAZE=!MAZE!!go!
 		)
@@ -42,21 +47,19 @@ For /L %%h IN (1, 1, %maxheight%) DO (
 	SET MAZE=
 
 	::pause screen
-	IF %pause% GTR 0 (timeout /t %pause% > nul)
+	IF %pause% GTR 0 (ping 1.1.1.1 -n 1 -w %pause% > nul)
 )
 
 echo Width=%width% height=%height% 
-echo bt_maze /? for more options
 GOTO EOF
 
 :HELP
-echo [Binary Tree Algorithm Maze Creator] @chazjn 26/06/2014. ver 1.0.
-echo Based on code here: http://weblog.jamisbuck.org/2011/2/7/maze-generation-algorithm-recap
-echo/
 echo Options:
 echo bt_maze [width] [height] [pauseTime]
-echo If no height specified, value of width will be used
-echo pauseTime set the time in seconds to pause between drawing rows
+echo If no height specified, value of width will be used.
+echo pauseTime set the (approximate) time in milliseconds to pause between drawing rows.
+echo All flags are optional.
+echo e.g.: bt_maze 50 75 900
 echo/
 
 :EOF
